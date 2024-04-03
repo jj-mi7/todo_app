7a6648bb5c1245c44cef39abcfb970d92304a79c
jj-mi7 <130371184+jj-mi7@users.noreply.github.com>
2024-04-03 18:26:27 +0530

    Update MainActivity.kt

--- app/src/main/java/com/www/todofinal/MainActivity.kt
+++ app/src/main/java/com/www/todofinal/MainActivity.kt
@@ -4,10 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import com.www.todofinal.data.roomdb.Todo
 import com.www.todofinal.ui.navigation.Navbar
 import com.www.todofinal.ui.theme.TodoFinalTheme
-import com.www.todofinal.viewModel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -17,7 +16,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TodoFinalTheme {
-                val homeViewModel:HomeViewModel by viewModels()
                 val intent = intent
                 val action = intent.action
                 val type = intent.type
@@ -25,7 +23,7 @@ class MainActivity : ComponentActivity() {
                 if (Intent.ACTION_SEND == action && "text/plain" == type) {
                     val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
                     if (sharedText != null) {
-                        homeViewModel.setIntentData(sharedText)
+                        intentobject.sharedText = sharedText
                     }
                 }
                 Navbar()
@@ -35,3 +33,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+object buffer {
+    lateinit var bufferTodo: Todo
+}
+
+object intentobject {
+    var sharedText = ""
+}
